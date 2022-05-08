4009a838644c134966fd49cfa9ab70733de3ff8a
nambarudayababu <99812857+nambarudayababu@users.noreply.github.com>
2022-05-08 08:51:01 +0530

    Create Asn2.py

---
+++ Asn2.py
@@ -0,0 +1,12 @@
+n=int(input( ))
+d={ }
+for i in range(n):
+    k=int(input( ))
+    v=int(input( ))
+    d[k]=v 
+for k in d:
+       if i*i==k:
+            print('yes',end=' ')
+       else:
+            print('no',end=' ')
+        
